95362be6315a35f6e5fc3fd06a91d7ee9deb838b
leijie <leijie@wondercareway.com>
2019-03-11 14:13:33 +0800

    修改配置，本地能直接调试

---
+++ src/main/java/debbuger/MycatStartupTest.java
@@ -0,0 +1,17 @@
+package debbuger;
+
+import io.mycat.MycatStartup;
+
+/**
+ * @author JieLei
+ * @date 2019/3/11
+ * @time 11:15
+ * @des
+ */
+public class MycatStartupTest {
+
+    public static void main(String[] args) {
+        MycatStartup.main(args);
+    }
+
+}
